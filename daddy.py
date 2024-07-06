0fd388d9468ff4ee3f904c77af9cc51df13a10c1
jackuiyw <jackwilliam324t@gmail.com>
2024-07-06 22:55:09 +0545

    Update daddy.py

--- daddy.py
+++ daddy.py
@@ -1,6 +1,6 @@
 import platform
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import mafia64_enc
+    import daddy64_enc
 elif bit == '32bit':
-    import mafia32enc_
+    import daddy32_enc
